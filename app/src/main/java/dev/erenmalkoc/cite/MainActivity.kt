300900d59e5dfa39496733e0ed85a00f6fc386ac
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-10-20 03:17:54 +0300

    handle intent in the activity

--- app/src/main/java/dev/erenmalkoc/cite/MainActivity.kt
+++ app/src/main/java/dev/erenmalkoc/cite/MainActivity.kt
@@ -1,6 +1,8 @@
 package dev.erenmalkoc.cite
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        handleIntent(intent)
+
+    }
+
+
+
+   private fun handleIntent (intent : Intent?){
+
+        if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+
+                findViewById<TextView>(R.id.textView).text = it.split("\\n[\\r]?".toRegex())[0]
+            }
+
         }
+
     }
 }
