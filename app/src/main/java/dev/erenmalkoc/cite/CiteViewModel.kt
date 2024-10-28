41447a7b280d0ee40b1e52b5e6c29d9fecdc154d
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-10-28 20:58:45 +0300

    added highlight stroke preview list.

--- app/src/main/java/dev/erenmalkoc/cite/CiteViewModel.kt
+++ app/src/main/java/dev/erenmalkoc/cite/CiteViewModel.kt
@@ -1,21 +1,46 @@
 package dev.erenmalkoc.cite
 
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.erenmalkoc.cite.preview.Preview
-import dev.erenmalkoc.cite.preview.previewList
+import dev.erenmalkoc.cite.preview.PreviewAdapter
 
-class CiteViewModel : ViewModel() {
+
+class CiteViewModel : ViewModel(), PreviewAdapter.PreviewItemClickListener {
 
     val cite = MutableLiveData("")
     val author = MutableLiveData("")
     val book = MutableLiveData("")
-    val previews = MutableLiveData(previewList)
+    val previews = MutableLiveData(
+        listOf(
+            Preview(
+                layoutId = R.layout.preview_1,
+                isSelected = true
+            ),
+            Preview(
+                layoutId = R.layout.preview_2,
+                isSelected = true
+            )
+
+
+        )
+    )
 
     fun onIntentWithTextExtra(textExtra: String) {
 
         cite.value = textExtra
+
+    }
+
+    override fun onItemClick(preview: Preview) {
+        previews.value = previews.value!!.map {
+            if (it.layoutId == preview.layoutId) {
+                preview.copy(isSelected = true)
+            } else {
+                it.copy(isSelected = false)
+            }
+        }
+
 
     }
 
