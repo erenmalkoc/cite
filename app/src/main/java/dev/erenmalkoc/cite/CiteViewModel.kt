e5d809656c25f10450fd76d16487ea72e16e3acc
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-11-19 18:55:59 +0300

    Share selected cite as a png file.

--- app/src/main/java/dev/erenmalkoc/cite/CiteViewModel.kt
+++ app/src/main/java/dev/erenmalkoc/cite/CiteViewModel.kt
@@ -2,6 +2,7 @@ package dev.erenmalkoc.cite
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.erenmalkoc.cite.preview.CiteInfo
 import dev.erenmalkoc.cite.preview.Preview
 import dev.erenmalkoc.cite.preview.PreviewAdapter
 
@@ -26,6 +27,15 @@ class CiteViewModel : ViewModel(), PreviewAdapter.PreviewItemClickListener {
         )
     )
 
+    var selectedPreview = previews.value!![0]
+
+    val citeInfo
+        get() = CiteInfo(
+            cite = cite.value!!,
+            author = author.value!!,
+            book = book.value!!
+        )
+
     fun onIntentWithTextExtra(textExtra: String) {
 
         cite.value = textExtra
@@ -40,8 +50,19 @@ class CiteViewModel : ViewModel(), PreviewAdapter.PreviewItemClickListener {
                 it.copy(isSelected = false)
             }
         }
+selectedPreview = preview
+
+    }
+
+    fun onShareCiteButtonClick() {
+// 1) Get a bitmap out of view hierarchy.
+
+
+// 2) Save the bitmap as s png file in temp directory.
+// 3) Share the url of the file with system dialog.
 
 
     }
+
 
 }
